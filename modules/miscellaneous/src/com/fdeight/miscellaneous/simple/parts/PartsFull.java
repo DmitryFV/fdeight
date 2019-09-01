619a4113498830b685b3bd6bdbb0c8655ad6760b
DmitryFV <dima747@mail.ru>
2019-09-01 14:00:28 +0600

    [*] miscellaneous: 2019.09.01 Parts, PartsTest, PartsFull - выделен класс PartsFull (подготовка на появлению других алгоритмов) с фабрикой создания экземпляра.

---
+++ modules/miscellaneous/src/com/fdeight/miscellaneous/simple/parts/PartsFull.java
@@ -0,0 +1,75 @@
+package com.fdeight.miscellaneous.simple.parts;
+
+import java.util.Arrays;
+
+public class PartsFull extends Parts {
+    static class PartsFullFactory extends PartsFactory {
+        @Override
+        Parts createParts(final int[] values, final int numParts) {
+            return new PartsFull(values, numParts);
+        }
+    }
+
+    static final PartsFactory factory = new PartsFullFactory();
+
+    /**
+     * Вспомогательный массив для оптимизации по времени работы.
+     * Получается выиграть немного (процентов 10), если не отводить память постоянно под этот массив,
+     * а заполнять его нулями.
+     */
+    private int[] sum;
+
+    private PartsFull(final int[] values, final int numParts) {
+        super(values, numParts);
+    }
+
+    @Override
+    int[] compute(final int[] values) {
+        results = new int[values.length];
+        Arrays.fill(results, -1);
+        sum = new int[numParts];
+        if (hasConditionsOfNoSolution()) {
+            return EMPTY_RESULT;
+        }
+        if (computeElement(0)) {
+            return results;
+        } else {
+            return EMPTY_RESULT;
+        }
+    }
+
+    private boolean computeElement(final int startIndex) {
+        if (startIndex == results.length) {
+            return checkTerminalSum();
+        } else {
+            if (values[startIndex] == 0) {
+                results[startIndex] = 0;
+                return computeElement(startIndex + 1);
+            }
+            // Данная оптимизация (maxPartNumber) немного замедляет (скорее всего, за счет лишнего вычисления минимума)
+            // в ситуациях, когда решение существует, но дает большой выигрыш (в разы), когда решения не существует
+            // (можем отсекать часть вариантов, симметричных ранее проверенным вариантам).
+            final int maxPartNumber = Math.min(startIndex, numParts - 1);
+            for (int part = 0; part <= maxPartNumber; part++) {
+                results[startIndex] = part;
+                if (computeElement(startIndex + 1)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    private boolean checkTerminalSum() {
+        Arrays.fill(sum, 0);
+        for (int i = 0; i < values.length; i++) {
+            sum[results[i]] += values[i];
+        }
+        for (int part = 0; part < numParts - 1; part++) {
+            if (sum[part] != sum[part + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- modules/miscellaneous/srctest/com/fdeight/miscellaneous/simple/parts/PartsTest.java
+++ modules/miscellaneous/srctest/com/fdeight/miscellaneous/simple/parts/PartsTest.java
@@ -4,14 +4,14 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class PartsTest {
-
     @Test
-    public void testCompute01() {
-        testCompute(new int[]{1, 2, 3, 4}, 2);
+    public void testFullCompute01() {
+        testCompute(PartsFull.factory, new int[]{1, 2, 3, 4}, 2);
     }
 
-    private void testCompute(final int[] values, final int numParts) {
-        final Parts parts = new Parts(values, numParts);
+    @SuppressWarnings("SameParameterValue")
+    private void testCompute(final Parts.PartsFactory partsFactory, final int[] values, final int numParts) {
+        final Parts parts = partsFactory.createParts(values, numParts);
         final int[] results = parts.compute(values);
         Assert.assertNotNull("results is null", results);
         Assert.assertEquals("values.length != results.length",
@@ -26,133 +26,133 @@ public class PartsTest {
     }
 
     @Test
-    public void testCompute02() {
-        testCompute(new int[]{1, 2, 6, 3}, 2);
+    public void testFullCompute02() {
+        testCompute(PartsFull.factory, new int[]{1, 2, 6, 3}, 2);
     }
 
     @Test
-    public void testCompute03() {
-        testCompute(new int[]{1, 2, 6, 3, 1, 5}, 3);
+    public void testFullCompute03() {
+        testCompute(PartsFull.factory, new int[]{1, 2, 6, 3, 1, 5}, 3);
     }
 
     @Test
-    public void testCompute04() {
-        testCompute(new int[]{1, 2, 6, 4, 1, 5, 3, 2}, 4);
+    public void testFullCompute04() {
+        testCompute(PartsFull.factory, new int[]{1, 2, 6, 4, 1, 5, 3, 2}, 4);
     }
 
     @Test
-    public void testCompute05() {
-        testCompute(new int[]{-20, -20}, 2);
+    public void testFullCompute05() {
+        testCompute(PartsFull.factory, new int[]{-20, -20}, 2);
     }
 
     @Test
-    public void testCompute06() {
-        testCompute(new int[]{-1, 2, 3, 6}, 2);
+    public void testFullCompute06() {
+        testCompute(PartsFull.factory, new int[]{-1, 2, 3, 6}, 2);
     }
 
     @Test
-    public void testCompute07() {
+    public void testFullCompute07() {
         final int[] values = new int[]{6, 20, 7, 6, 5, 6, -4, 5, 14, -1, 10, 24, -5, 7};
         final int numParts = 5;
-        testCompute(values, numParts);
+        testCompute(PartsFull.factory, values, numParts);
     }
 
     @Test
-    public void testNotComputed01() {
+    public void testFullNotComputed01() {
         final int[] values = new int[]{1, 2, 3, 5};
         final int numParts = 2;
-        testNotComputed(values, numParts);
+        testNotComputed(PartsFull.factory, values, numParts);
     }
 
-    private void testNotComputed(final int[] values, final int numParts) {
-        final Parts parts = new Parts(values, numParts);
+    @SuppressWarnings("SameParameterValue")
+    private void testNotComputed(final Parts.PartsFactory partsFactory, final int[] values, final int numParts) {
+        final Parts parts = partsFactory.createParts(values, numParts);
         final int[] results = parts.compute(values);
         Assert.assertNotNull("results is null", results);
         Assert.assertArrayEquals("results is not empty", new int[]{}, results);
     }
 
-
     @Test
-    public void testNotComputed02() {
+    public void testFullNotComputed02() {
         final int[] values = new int[]{1, 2, 6, 8};
         final int numParts = 2;
-        testNotComputed(values, numParts);
+        testNotComputed(PartsFull.factory, values, numParts);
     }
 
     @Test
-    public void testNotComputede03() {
+    public void testFullNotComputed03() {
         final int[] values = new int[]{2, 2, 6, 3, 1, 5};
         final int numParts = 3;
-        testNotComputed(values, numParts);
+        testNotComputed(PartsFull.factory, values, numParts);
     }
 
     @Test
-    public void testNotComputed04() {
+    public void testFullNotComputed04() {
         final int[] values = new int[]{1, 2, 6, 4, -1, 5, 3, 2};
         final int numParts = 4;
-        testNotComputed(values, numParts);
+        testNotComputed(PartsFull.factory, values, numParts);
     }
 
     @Test
-    public void testNotComputed05() {
+    public void testFullNotComputed05() {
         final int[] values = new int[]{-20, -21};
         final int numParts = 2;
-        testNotComputed(values, numParts);
+        testNotComputed(PartsFull.factory, values, numParts);
     }
 
     @Test
-    public void testNotComputed06() {
+    public void testFullNotComputed06() {
         final int[] values = new int[]{-1, -3, 3, 6};
         final int numParts = 2;
-        testNotComputed(values, numParts);
+        testNotComputed(PartsFull.factory, values, numParts);
     }
 
     @Test
-    public void testNotComputed07() {
+    public void testFullNotComputed07() {
         final int[] values = new int[]{-1, -2, 3, 6};
         final int numParts = 3;
-        testNotComputed(values, numParts);
+        testNotComputed(PartsFull.factory, values, numParts);
     }
 
     @Test
-    public void testNotComputed08() {
+    public void testFullNotComputed08() {
         final int[] values = new int[]{0, -1, -2, 3, 6, -4, 7, -9, 12, 5, -2, 1, 2, 0, 0, 3, 4, 16, 0};
         final int numParts = 3;
-        testNotComputed(values, numParts);
+        testNotComputed(PartsFull.factory, values, numParts);
     }
 
     @Test
-    public void testNotComputed09() {
+    public void testFullNotComputed09() {
         final int[] values = new int[]{10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
         final int numParts = 4;
-        testNotComputed(values, numParts);
+        testNotComputed(PartsFull.factory, values, numParts);
     }
 
     @Test
-    public void testNotComputed10() {
+    public void testFullNotComputed10() {
         final int[] values = new int[]{10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
         final int numParts = 4;
-        testNotComputed(values, numParts);
+        testNotComputed(PartsFull.factory, values, numParts);
     }
 
     @Test
-    public void testNotComputed11() {
+    public void testFullNotComputed11() {
         final int[] values = new int[]{5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
         final int numParts = 4;
-        testNotComputed(values, numParts);
+        testNotComputed(PartsFull.factory, values, numParts);
     }
 
     @Test
-    public void testNotComputed12() {
+    public void testFullNotComputed12() {
         final int[] values = new int[]{5, 5, 3, 3, 3, 3, 3, 5, 5, 3, 3, 3, 3, 3};
         final int numParts = 5;
-        testNotComputed(values, numParts);
+        testNotComputed(PartsFull.factory, values, numParts);
     }
 
     @Test
-    public void testNotComputed13() {
+    public void testFullNotComputed13() {
         final int[] values = new int[]{5, 5, 0, 0, 0, 0, 0, 5, 5, 3, 3, 3, 3, 3};
         final int numParts = 5;
-        testNotComputed(values, numParts);
+        testNotComputed(PartsFull.factory, values, numParts);
     }
 }
