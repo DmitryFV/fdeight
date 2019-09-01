b507b77018ef3c81a18346517cf856759178f014
DmitryFV <dima747@mail.ru>
2019-09-02 04:15:51 +0600

    [*] miscellaneous: 2019.09.01 PartsTest - в тестах удалены лишние переменные.

--- modules/miscellaneous/srctest/com/fdeight/miscellaneous/simple/parts/PartsTest.java
+++ modules/miscellaneous/srctest/com/fdeight/miscellaneous/simple/parts/PartsTest.java
@@ -56,16 +56,14 @@ public class PartsTest {
 
     @Test
     public void testFullComputed07() {
-        final int[] values = new int[]{6, 20, 7, 6, 5, 0, 0, 0, 6, -4, 5, 14, -1, 10, 24, -5, 7};
-        final int numParts = 5;
-        testComputed(PartsFull.factory, values, numParts);
+        testComputed(PartsFull.factory,
+                new int[]{6, 20, 7, 6, 5, 0, 0, 0, 6, -4, 5, 14, -1, 10, 24, -5, 7},
+                5);
     }
 
     @Test
     public void testFullComputed08() {
-        final int[] values = new int[]{-2, -1, -3, -8, -5, -1, -4, -6, -3, -6, -10, -2, -2, -2};
-        final int numParts = 5;
-        testComputed(PartsFull.factory, values, numParts);
+        testComputed(PartsFull.factory, new int[]{-2, -1, -3, -8, -5, -1, -4, -6, -3, -6, -10, -2, -2, -2}, 5);
     }
 
     @Test
@@ -87,9 +85,7 @@ public class PartsTest {
 
     @Test
     public void testFullNotComputed01() {
-        final int[] values = new int[]{1, 2, 3, 5};
-        final int numParts = 2;
-        testNotComputed(PartsFull.factory, values, numParts);
+        testNotComputed(PartsFull.factory, new int[]{1, 2, 3, 5}, 2);
     }
 
     private void testNotComputed(final Parts.PartsFactory partsFactory, final int[] values, final int numParts) {
@@ -101,86 +97,69 @@ public class PartsTest {
 
     @Test
     public void testFullNotComputed02() {
-        final int[] values = new int[]{1, 2, 6, 8};
-        final int numParts = 2;
-        testNotComputed(PartsFull.factory, values, numParts);
+        testNotComputed(PartsFull.factory, new int[]{1, 2, 6, 8}, 2);
     }
 
     @Test
     public void testFullNotComputed03() {
-        final int[] values = new int[]{2, 2, 6, 3, 1, 5};
-        final int numParts = 3;
-        testNotComputed(PartsFull.factory, values, numParts);
+        testNotComputed(PartsFull.factory, new int[]{2, 2, 6, 3, 1, 5}, 3);
     }
 
     @Test
     public void testFullNotComputed04() {
-        final int[] values = new int[]{1, 2, 6, 4, -1, 5, 3, 2};
-        final int numParts = 4;
-        testNotComputed(PartsFull.factory, values, numParts);
+        testNotComputed(PartsFull.factory, new int[]{1, 2, 6, 4, -1, 5, 3, 2}, 4);
     }
 
     @Test
     public void testFullNotComputed05() {
-        final int[] values = new int[]{-20, -21};
-        final int numParts = 2;
-        testNotComputed(PartsFull.factory, values, numParts);
+        testNotComputed(PartsFull.factory, new int[]{-20, -21}, 2);
     }
 
     @Test
     public void testFullNotComputed06() {
-        final int[] values = new int[]{-1, -3, 3, 6};
-        final int numParts = 2;
-        testNotComputed(PartsFull.factory, values, numParts);
+        testNotComputed(PartsFull.factory, new int[]{-1, -3, 3, 6}, 2);
     }
 
     @Test
     public void testFullNotComputed07() {
-        final int[] values = new int[]{-1, -2, 3, 6};
-        final int numParts = 3;
-        testNotComputed(PartsFull.factory, values, numParts);
+        testNotComputed(PartsFull.factory, new int[]{-1, -2, 3, 6}, 3);
     }
 
     @Test
     public void testFullNotComputed08() {
-        final int[] values = new int[]{0, -1, -2, 3, 6, -4, 7, -9, 12, 5, -2, 1, 2, 0, 0, 3, 4, 16, 0};
-        final int numParts = 3;
-        testNotComputed(PartsFull.factory, values, numParts);
+        testNotComputed(PartsFull.factory,
+                new int[]{0, -1, -2, 3, 6, -4, 7, -9, 12, 5, -2, 1, 2, 0, 0, 3, 4, 16, 0},
+                3);
     }
 
     @Test
     public void testFullNotComputed09() {
-        final int[] values = new int[]{10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-        final int numParts = 4;
-        testNotComputed(PartsFull.factory, values, numParts);
+        testNotComputed(PartsFull.factory, new int[]{10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 4);
     }
 
     @Test
     public void testFullNotComputed10() {
-        final int[] values = new int[]{10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-        final int numParts = 4;
-        testNotComputed(PartsFull.factory, values, numParts);
+        testNotComputed(PartsFull.factory, new int[]{10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 4);
     }
 
     @Test
     public void testFullNotComputed11() {
-        final int[] values = new int[]{5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-        final int numParts = 4;
-        testNotComputed(PartsFull.factory, values, numParts);
+        testNotComputed(PartsFull.factory, new int[]{5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 4);
     }
 
     @Test
     public void testFullNotComputed12() {
-        final int[] values = new int[]{4, 4, 3, 3, 3, 3, 4, 4, 3, 3, 3, 3, 5};
-        final int numParts = 5;
-        testNotComputed(PartsFull.factory, values, numParts);
+        testNotComputed(PartsFull.factory, new int[]{4, 4, 3, 3, 3, 3, 4, 4, 3, 3, 3, 3, 5}, 5);
     }
 
     @Test
     public void testFullNotComputed13() {
-        final int[] values = new int[]{5, 5, 0, 0, 0, 0, 0, 5, 5, 3, 3, 3, 3, 3};
-        final int numParts = 5;
-        testNotComputed(PartsFull.factory, values, numParts);
+        testNotComputed(PartsFull.factory, new int[]{5, 5, 0, 0, 0, 0, 0, 5, 5, 3, 3, 3, 3, 3}, 5);
+    }
+
+    @Test
+    public void testFullNotComputed14() {
+        testNotComputed(PartsFull.factory, new int[]{5, 5, 0, 0, 0, 0, 0, 5, 5, 3, 3, 3, 3, 3, 1}, 6);
     }
 
     @Test
@@ -249,9 +228,7 @@ public class PartsTest {
 
     @Test
     public void testDynamicNotComputed01() {
-        final int[] values = new int[]{1, 2, 3, 5};
-        final int numParts = 2;
-        testNotComputed(PartsDynamic.factory, values, numParts);
+        testNotComputed(PartsDynamic.factory, new int[]{1, 2, 3, 5}, 2);
     }
 
     @Test
